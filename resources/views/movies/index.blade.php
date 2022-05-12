4f67f488ebb0c82066d24af67017a4d32d42db30
SimoneScapin <simonescap87@gmail.com>
2022-05-12 15:32:31 +0200

    da sistemare layout

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,11 +2,22 @@
 
 @section('title', 'Movies')
 
-@section
+@section('main-content')
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <p></p>
+            @foreach ($movies as $movie)
+                <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <h6 class="card-text">{{$movie->original_title}}</h6>
+                    <h6 class="card-text">{{$movie->nationality}}</h6>
+                    <h6 class="card-text">{{$movie->date}}</h6>
+                    <h6 class="card-text">{{$movie->vote}}</h6>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
