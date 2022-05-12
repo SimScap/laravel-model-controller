65827839fb565f4c52a422fc6e043acb3751ea7b
SimoneScapin <simonescap87@gmail.com>
2022-05-12 16:11:16 +0200

    done

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,8 +8,7 @@
         <div class="row">
             <div class="col-12 text-center d-flex flex-wrap justify-content-center">
                 @foreach ($movies as $movie)
-                    <div class="card m-1" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
+                    <div class="card m-1">
                     <div class="card-body">
                         <h5 class="card-title">{{$movie->title}}</h5>
                         <h6 class="card-text">{{$movie->original_title}}</h6>
