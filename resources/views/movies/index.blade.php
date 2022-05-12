e23d70419be8889dcc46b5ba9139a846d1ff4cc1
SimoneScapin <simonescap87@gmail.com>
2022-05-12 15:47:14 +0200

    layout da terminare

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,24 +3,24 @@
 @section('title', 'Movies')
 
 @section('main-content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            @foreach ($movies as $movie)
-                <div class="card" style="width: 18rem;">
-                <img src="" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">{{$movie->title}}</h5>
-                    <h6 class="card-text">{{$movie->original_title}}</h6>
-                    <h6 class="card-text">{{$movie->nationality}}</h6>
-                    <h6 class="card-text">{{$movie->date}}</h6>
-                    <h6 class="card-text">{{$movie->vote}}</h6>
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center d-flex flex-wrap justify-content-center">
+                @foreach ($movies as $movie)
+                    <div class="card m-1" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <h6 class="card-text">{{$movie->original_title}}</h6>
+                        <h6 class="card-text">{{$movie->nationality}}</h6>
+                        <h6 class="card-text">{{$movie->date}}</h6>
+                        <h6 class="card-text">{{$movie->vote}}</h6>
+                    </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
-</div>
-
-
+</main>
 @endsection
